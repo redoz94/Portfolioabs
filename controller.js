652a41ec908544d596113e4a619016323371f334
redoz94 <azfarbil75@gmail.com>
2025-04-25 17:29:36 +0500

    25-4-25

--- controller.js
+++ controller.js
@@ -5,6 +5,9 @@ window.onload = () => {
   
   const toggle = e => e.classList.toggle('is-active');
   const toggleNav = ({ target }) => Array.from(navMenu.classList).includes('is-active') ? toggle(navMenu) : null;
+  const navToggle = document.querySelector('.nav-toggle');
+
+  
 
   hamburger.addEventListener('click', () => toggle(navMenu, 'is-active'));
   Array.from(navItems).forEach(e => e.addEventListener('click', toggleNav));
@@ -18,3 +21,26 @@ document.addEventListener('DOMContentLoaded', function() {
       navMenu.classList.toggle('is-active');
   });
 });
+
+const navToggle = document.getElementById('nav-toggle');
+        const navMenu = document.querySelector('.nav-menu');
+
+        navToggle.addEventListener('click', () => {
+            navMenu.classList.toggle('is-active');
+        });
+
+function enlargeImage(img) {
+  var newWidth = img.width * 2; // Double the width
+  var newHeight = img.height * 2; // Double the height
+  img.style.width = newWidth + "px";
+  img.style.height = newHeight + "px";
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
