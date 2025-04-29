1f97b2dd70b97d60a8d34f6b0f159fa284d01d17
redoz94 <azfarbil75@gmail.com>
2025-04-29 17:45:15 +0500

    flex

--- controller.js
+++ controller.js
@@ -1,46 +1,9 @@
-window.onload = () => {
-  const navMenu = document.querySelector('.nav-menu');
-  const navItems = document.querySelectorAll('.nav-item');
-  const hamburger = document.querySelector('.nav-toggle');
-  
-  const toggle = e => e.classList.toggle('is-active');
-  const toggleNav = ({ target }) => Array.from(navMenu.classList).includes('is-active') ? toggle(navMenu) : null;
-  const navToggle = document.querySelector('.nav-toggle');
-
-  
-
-  hamburger.addEventListener('click', () => toggle(navMenu, 'is-active'));
-  Array.from(navItems).forEach(e => e.addEventListener('click', toggleNav));
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  const navToggle = document.getElementById('nav-toggle');
-  const navMenu = document.querySelector('.nav-menu');
-
-  navToggle.addEventListener('click', () => {
-      navMenu.classList.toggle('is-active');
-  });
-});
-
-const navToggle = document.getElementById('nav-toggle');
-        const navMenu = document.querySelector('.nav-menu');
-
-        navToggle.addEventListener('click', () => {
-            navMenu.classList.toggle('is-active');
-        });
-
-function enlargeImage(img) {
-  var newWidth = img.width * 2; // Double the width
-  var newHeight = img.height * 2; // Double the height
-  img.style.width = newWidth + "px";
-  img.style.height = newHeight + "px";
-}
-
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+document.addEventListener('DOMContentLoaded', () => { const navMenu = document.querySelector('.nav-menu'); const navItems = document.querySelectorAll('.nav-item'); const navToggle = document.getElementById('nav-toggle'); 
+   const toggleActiveClass = (element) => { if (element) { element.classList.toggle('is-active'); } }; 
+    const handleNavItemClick = () => { if (navMenu && navMenu.classList.contains('is-active')) { toggleActiveClass(navMenu); } }; 
+     if (navToggle) { navToggle.addEventListener('click', () => { toggleActiveClass(navMenu); }); } 
+      navItems.forEach((navItem) => { navItem.addEventListener('click', handleNavItemClick); }); 
+      function enlargeImage(img) { const newWidth = img.width * 2; const newHeight = img.height * 2; img.style.width = `${newWidth}px`; 
+       img.style.height = `${newHeight}px`; } 
+       function myFunction() { const myLinks = document.getElementById('myLinks'); if (myLinks) {
+         if (myLinks.style.display === 'block') { myLinks.style.display = 'none'; } else { myLinks.style.display = 'block'; } } } });
